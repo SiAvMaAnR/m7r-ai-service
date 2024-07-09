99ff92a26d15b4aba732f9326f9c446f3600cd55
SiAvMaAnR <siavmaanr@gmail.com>
2024-07-09 23:14:54 +0300

    chore: setup launch.json

--- src/ai-core/ai-core.controller.ts
+++ src/ai-core/ai-core.controller.ts
@@ -1,5 +1,5 @@
-import { Controller } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
+import { Body, Controller, Post } from '@nestjs/common';
+// import { MessagePattern } from '@nestjs/microservices';
 import { AiCoreService } from './ai-core.service';
 import { CreateCompletionDto } from './dto/create-completion.dto';
 
@@ -7,8 +7,9 @@ import { CreateCompletionDto } from './dto/create-completion.dto';
 export class AiCoreController {
   constructor(private readonly aiCoreService: AiCoreService) {}
 
-  @MessagePattern('create-message')
-  async createMessage(createCompletionDto: CreateCompletionDto) {
+  @Post('create-message') // =================================================== temp
+  // @MessagePattern('create-message')
+  async createMessage(@Body() createCompletionDto: CreateCompletionDto) {
     return this.aiCoreService.createCompletion(createCompletionDto);
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { MicroserviceOptions } from '@nestjs/microservices';
 import { rabbitMQConfig } from './common/common.broker';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
-    AppModule,
-    rabbitMQConfig,
-  );
+  // const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+  //   AppModule,
+  //   rabbitMQConfig,
+  // );
 
-  await app.listen();
+  // await app.listen();
+
+  const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>(rabbitMQConfig);
+
+  await app.startAllMicroservices();
+  await app.listen(3001);
 }
 bootstrap();
