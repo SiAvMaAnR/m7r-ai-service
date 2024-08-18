dca00f66dcd6c6a06c5cad140f3c6641bec0292d
SiAvMaAnR <siavmaanr@gmail.com>
2024-08-19 00:01:49 +0300

    feat: timer middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,12 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AiCoreModule } from './ai-core/ai-core.module';
+import { TimerMiddleware } from './common/middlewares/timer.middleware';
 
 @Module({
   imports: [AiCoreModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(TimerMiddleware).forRoutes('*');
+  }
+}
