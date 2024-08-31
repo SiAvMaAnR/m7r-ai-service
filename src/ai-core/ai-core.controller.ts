f981fc282f14b62096b97fc325be540fef1001a4
SiAvMaAnR <siavmaanr@gmail.com>
2024-09-01 02:58:21 +0300

    feat: swagger config

--- src/ai-core/ai-core.controller.ts
+++ src/ai-core/ai-core.controller.ts
@@ -1,9 +1,9 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AiCoreService } from './ai-core.service';
 import { CreateCompletionDto } from './dto/create-completion.dto';
-import { CustomEventPattern } from 'src/common/common.decorators';
+import { CustomEventPattern } from '../common/common.decorators';
 
-@Controller()
+@Controller('api/ai-core')
 export class AiCoreController {
   constructor(private readonly aiCoreService: AiCoreService) {}
 
